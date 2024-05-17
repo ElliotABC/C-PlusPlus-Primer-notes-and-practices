d610412b7daa69ae86da5613bac7b0b2775d438f
DearIce <56225620+DearIcer@users.noreply.github.com>
2024-05-17 17:21:51 +0800

    好玩的

---
+++ 好玩的/Form1.cs
@@ -0,0 +1,18 @@
+namespace 好玩的
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            BackColor = Color.White;
+            TransparencyKey = Color.White;
+            pictureBox1.BackColor = TransparencyKey;
+        }
+    }
+}
