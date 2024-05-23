f8a6cea431529579ff79d7a1897f170c46d79cb3
Elliot <3271678446@qq.com>
2024-05-23 18:58:12 +0800

    标识符，名字的作用域，复合类型：引用，指针

---
+++ Chapter2/Practice11.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+/*
+2.2.3练习
+a:int double = 3.14;	不合法，标识符为double，数据类型为int，但赋值是float
+b：int _;不合法，函数外不能以下划线开头
+c：int catch-22；不合法，-不应该存在
+d：int 1_or_2 = 1;不合法，一个标识符不应该出现两个下划线_
+e：double Double = 3.14;合法，标识符为Double，数据类型为double，赋值为double
+*/
+
+/*
+2.3.1练习
+2.15
+a:int ival = 1.01不合法，整型但是赋值float
+b:
+*/
