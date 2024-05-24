7e89940a0e0fc2cecebb2c9779eb37c311e94987
DearIce <56225620+DearIcer@users.noreply.github.com>
2024-05-24 15:53:16 +0800

    指针

---
+++ 指针/指针.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a = 10;
+	int* p = &a;
+	int** pp = &p;
+	int*** ppp = &pp;
+
+	std::cout << "***ppp三级指针 == " << ***ppp << std::endl;
+	std::cout << "**pp二级指针 == " << **pp << std::endl;
+	std::cout << "*p一级指针 == " << *p << std::endl;
+	std::cout << "a == " << a << std::endl;
+
+	std::system("pause");
+}
