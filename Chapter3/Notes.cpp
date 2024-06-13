740b18fac1379d79f0af71f7631c88f49950574e
Elliot <3271678446@qq.com>
2024-06-13 11:13:21 +0800

    标准库类型string

--- Chapter3/Notes.cpp
+++ Chapter3/Notes.cpp
@@ -43,6 +43,42 @@
 	第2种方法
 	编译器应该从操作符(::)左侧名字所示的作用域中寻找右侧名字
 		std::cin使用作用域操作符（::）：使用命名空间std中的名字cin
-	
-	
+	 
+标准库类型stringg：可变长的字符序列
+	必须包含string头文件(在命名空间std中)
+	#include <string>
+	using std::string;
+
+	定义和初始化string对象
+		string s1;			//默认初始化，s1是一个空字符串
+		string s2 = s1;		//拷贝初始化，右侧初始值拷贝到新建对象。s2是s1的副本
+		string s2(s1)		//直接初始化
+		string s3 = "hiya";	//拷贝初始化，右侧初始值拷贝到新建对象。s3是字符串字面值的副本
+		string s3("hiya")	//直接初始化
+		string s4(10,'c');	//s4的内容是cccccccccc
+
+		string s4 = string(10,'c');	//s4初始值是string(10,'c'),string对象为用数字10和字符c
+		等价于
+		string temp(10,'c');	//temp内容是cccccccccc
+		string s4 = temp;		//将temp拷贝给s4
+	string对象上的操作
+		os<<s			s写入到输出流os中，返回os
+		is>>s			从is读取字符串并赋值给s，字符串以空白分隔，返回is		
+		getline(is,s)	从is读取一行赋值给s，返回is
+		s.empty()		s为空返回true，否则为false
+		s.size()		返回s中字符的个数
+		s[n]			返回s中第n个字符的引用，位置n从0开始
+		s1+s2			返回s1和s2连接的结果
+		s1=s2			用s2副本替代s1原来字符
+		s1==s2			如果s1和s2所含字符完全一样，则他们相等
+		s1!=s2			如果s1和s2所含字符不一样，则他们不相等
+		<,<=,>,>=		利用字符在字典中的顺序比较
+
+		读取string对象
+			使用IO操作符读写string对象
+				eg：
+				#include <iostream>
+				#include <string>
+				using std::cin
+				using std::cout
 */
--- Chapter3/Practice1.cpp
+++ Chapter3/Practice1.cpp
@@ -1,7 +1,4 @@
-﻿// Chapter3.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include <iostream>
 using std::cin;
 using std::cout;
 using std::endl;
